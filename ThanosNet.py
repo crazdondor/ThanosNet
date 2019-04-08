2a5d06db42a2e92adfc9c1a36f0472687d979996
Brewer Slack <bslack@zagmail.gonzaga.edu>
2019-04-08 15:56:41 -0700

    you are given a choice

--- ThanosNet.py
+++ ThanosNet.py
@@ -1,4 +1,8 @@
 def main():
     # initial commit
-
+    print("Welcome to ThanosNet")
+    print("1) Check if user exists")
+    print("2) Check connection between users")
+    print("3) Quit")
+    choice = input("Select the number of command you want")
 main()
