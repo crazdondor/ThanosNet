00c003f50fe1fabc98e8d841753d3f26cccbf326
Brewer Slack <bslack@zagmail.gonzaga.edu>
2019-04-08 17:14:42 -0700

    deliverable 1 finished

--- ThanosNet.py
+++ ThanosNet.py
@@ -1,14 +1,37 @@
 import csv
+from graph import Graph
 
+def check_user(graph):
+    user = input("Input user name: ")
+    print(graph.user_exists(user))
 
+def check_connection(graph):
+    user1 = input("Input user 1 name: ")
+    user2 = input("Input user 2 name: ")
+    val = graph.check_connection(user1, user2)
+    print(val)
 
 def main():
+    graph = Graph()
     # load .txt file
     with open('graph.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=' ')
+        # create list of users
         for row in readCSV:
-            # read the boi
-            pass
+            graph.add_user(row[0])
+            graph.add_user(row[1])
+        # initialize adj matrix
+        graph.init_adjmatrix()
+
+    with open('graph.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=' ')
+        # populate adj matrix
+        for row in readCSV:
+            val = int(row[2])
+            name1 = row[0]
+            name2 = row[1]
+            graph.add_val(name1, name2, val)
+
     print("Welcome to ThanosNet")
     choice = 0
     while choice != 3:
@@ -17,4 +40,8 @@ def main():
         print("2) Check connection between users")
         print("3) Quit")
         choice = int(input(">  "))
+        if choice == 1:
+            check_user(graph)
+        elif choice == 2:
+            check_connection(graph)
 main()
--- graph.py
+++ graph.py
@@ -8,3 +8,34 @@ class Graph:
         if user in self.users:
             return True
         return False
+    
+    def check_connection(self,user1, user2):
+        name1_val = self.find_nameval(user1)
+        name2_val = self.find_nameval(user2)
+        for i, row in enumerate(self.adj_matrix):
+            if i == name1_val:
+                for j, col in enumerate(self.adj_matrix):
+                    if j == name2_val:
+                        return self.adj_matrix[i][j]
+                        print("benis")
+
+    def init_adjmatrix(self):
+        self.adj_matrix = [[0 for i in range(len(self.users))] for j in range(len(self.users))]
+
+    def add_user(self, user):
+        if not self.user_exists(user):
+            self.users.append(user)
+
+    def find_nameval(self, name):
+        for i in range(len(self.users)):
+            if self.users[i] == name:
+                return i
+
+    def add_val(self, name1, name2, val):
+        name1_val = self.find_nameval(name1)
+        name2_val = self.find_nameval(name2)
+        for i, row in enumerate(self.adj_matrix):
+            if i == name1_val:
+                for j, col in enumerate(self.adj_matrix):
+                    if j == name2_val:
+                        self.adj_matrix[i][j] = val
